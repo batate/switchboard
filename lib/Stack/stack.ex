3a41c0319609ea547b4bac8ab1087632c0c386a0
Bruce Tate <bruce@rapidred.com>
2013-11-22 13:54:55 -0600

    handlers for explicit return codes

--- lib/Stack/stack.ex
+++ lib/Stack/stack.ex
@@ -1,17 +1,74 @@
-defrecord Switchboard.Stack, name: nil, plugs: [] do
-  def call(context, stack), 
-    do: _call({:ok, context}, stack.plugs)
-
-  defp _call({:ok, context}, [plug|tail]), 
-    do: _call(plug.call(context), tail) 
-    
-  defp _call({:ok, context}, []), 
-    do: ({:ok, context})
-    
-  defp _call({:halt, context}, _), 
-    do: ({:halt, context})
+defrecord Switchboard.Stack, name: nil, plugs: [], handlers: [] do
+  @type name              :: atom
+  @type plugs             :: [ Switchboard.Plug ]
+  @type handlers          :: [ {atom, module} ]
   
-  def add(new_plug, stack), 
-    do: Switchboard.Stack.new name: stack.name, plugs: stack.plugs ++ [new_plug]
+  @moduledoc """
+  Stacks
+  
+  A stack is a composition of plugs. Stack implements Switchboard.Plug, so you can invoke it just as you would a plug.
+  
+  ## Calling a stack
+  
+  
+  """
+
+  @doc """
+  Calls this stack. 
+
+  Consider a stack with the plugs [plug1, plug2, plug3],
+  all of which return {:ok, context}. 
+  
+  
+  context |> plug1 |> plug2 |> plug3
+  
+  The traversal will halt if the result is anything other than :ok. 
+  Consider the stack with the plugs [plug1, plug2, plug3], 
+  where plug2 returns {:halt, context}. In this case, invoking the 
+  stack would give you the composition:
+  
+  context |> plug1 |> plug 2
+  
+  """
+  def call(context, stack), do: _call({:ok, context}, stack.plugs, stack)
+
+  defp _call({:ok, context}, [plug|tail], stack) do
+    _call(plug.call(context), tail, stack) 
+  end
+    
+  defp _call({:ok, context}, [], _), do: {:ok, context}
+  defp _call({:halt, context}, _, _), do: {:halt, context}
+  defp _call(result, _, stack), do: stack.handle(result)
+
+  def handle({code, context}, stack) do
+    handler = Keyword.get stack.handlers, code
+    _handle code, context, handler
+  end
+  
+  defp _handle(code, context, nil), do: {code, context}
+  defp _handle(code, context, plug), do: plug.call context 
+
+
+  @doc """
+  Returns a new stack with a plug appended to the end of plugs.
+  """
+  def add_plug(plug, stack) do
+    Switchboard.Stack.new name: stack.name, 
+                          plugs: stack.plugs ++ [plug], 
+                          handlers: stack.handlers
+  end
+
+  @doc """
+  Add a new handler to the stack
+  """
+  def add_handler(handler, stack) do
+    if handler.name == nil, do: raise "A stack must have a name to be a handler"
+    Switchboard.Stack.new name: stack.name, 
+                          plugs: stack.plugs, 
+                          handlers: stack.handlers ++ [{binary_to_atom( handler.name ), handler}]
+  end
+
+
+  
   
 end
--- test/switchboard_test.exs
+++ test/switchboard_test.exs
@@ -4,10 +4,17 @@ defmodule SwitchboardTest do
 
   def inc(int), do: {:ok, int + 1}
   def double(int), do: {:ok, int * 2}
+  def halt(int), do: {:halt, int}
 
   def simple_plug, do: Switchboard.Plug.Fun.new function_name: :inc, module: SwitchboardTest
   def double_plug, do: Switchboard.Plug.Fun.new function_name: :double, module: SwitchboardTest
+  def halt_plug, do: Switchboard.Plug.Fun.new function_name: :halt, module: SwitchboardTest
   def stack, do: Switchboard.Stack.new plugs: [simple_plug, double_plug, double_plug]
+  def halt_stack, do: Switchboard.Stack.new plugs: [simple_plug, halt_plug, double_plug]
+  def handler, do: Switchboard.Stack.new plugs: [double_plug], name: "double"
+  def stack_with_handler do
+    Switchboard.Stack.new plugs: [double_plug], name: "handler", handlers: [double: handler]
+  end
   
   test "should call simple plug", 
     do: assert( simple_plug.call(0) == {:ok, 1})
@@ -15,10 +22,22 @@ defmodule SwitchboardTest do
   test "should call plug with module", 
     do: assert( double_plug.call(1) == {:ok, 2})
   
+  test "should halt stack",
+    do: assert( halt_stack.call(0) == {:halt, 1} )
+    
   test "should call stack",
     do: assert( stack.call(2) == {:ok, 12} )
     
   test "should add to plug stack", 
-    do: assert( (stack.add(double_plug).plugs |> Enum.count) == 4 )
+    do: assert( (stack.add_plug(double_plug).plugs |> Enum.count) == 4 )
+
+  test "should add a handler to stack" do
+    added = stack.add_handler(handler)
+    assert (added.handlers |> Enum.count) == 1
+    assert Keyword.get( added.handlers, :double ).name == "double"
+  end
+    
+  test "should handle tuple", 
+    do: assert( stack_with_handler.handle({:double, 1}) == {:ok, 2})
   
 end
